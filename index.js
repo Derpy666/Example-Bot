d6d9b9d0a7d70cf3e06c5ecc0cb9fa2154d60804
Derpy666 <37512753+Derpy666@users.noreply.github.com>
2018-04-01 16:57:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -88,31 +88,4 @@ client.on("message", (message) => {
     }
   });
 
-    if(command === "kick") {
-      // This command must be limited to mods and admins. In this example we just hardcode the role names.
-      // Please read on Array.some() to understand this bit: 
-      // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/some?
-      if(!message.member.roles.some(r=>["Administrator", "Moderator"].includes(r.name)) )
-        return message.reply("Sorry, you don't have permissions to use this!");
-    
-      // Let's first check if we have a member and if we can kick them!
-      // message.mentions.members is a collection of people that have been mentioned, as GuildMembers.
-      let member = message.mentions.members.first();
-      if(!member)
-        return message.reply("Please mention a valid member of this server");
-      if(!member.kickable) 
-        return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
-    
-      // slice(1) removes the first part, which here should be the user mention!
-      let reason = args.slice(1).join(' ');
-      if(!reason)
-        return message.reply("Please indicate a reason for the kick!");
-    
-      // Now, time for a swift kick in the nuts!
-      await member.kick(reason)
-        .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
-      message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
-
-    }
-
 client.login(process.env.BOT_TOKEN);
