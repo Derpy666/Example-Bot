16d77f0f7ef7afe63b1113fb48a642e379212417
Derpy666 <37512753+Derpy666@users.noreply.github.com>
2018-05-25 16:36:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -17,24 +17,17 @@ bot.on("guildDelete", guild => {
 bot.user.setActivity(`/help | Mad by Derpy`, {type: "PLAYING"});
 });
 
-bot.on("message", async message => {
-  if(message.author.bot) return;
-  if(message.channel.type === "dm") return; 
+bot.on("message", async msg => {
+  if(msg.author.bot) return;
+  if(msg.channel.type === "dm") return; 
 
   let prefix = botconfig.prefix;
-  let messageArray = message.content.split(" ");
+  let messageArray = msg.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
   let rainbow = ((1 << 24) * Math.random() | 0).toString(16); 
 
-  //bot.on('ready', () => console.log('Bot is Ready!')); 
-  //bot.on('message', message => console.log('Bot is Ready!')); 
-  //let readyChannel = message.guild.channels.find(`name`, "bot-console");  
-  //let readyMessage = message.channel.send("Bot is now Online!");
-  //readyChannel.send(readyMessage);
-
   if(cmd === `${prefix}help`){
-
     let bicon = bot.user.displayAvatarURL;
     let botembed = new Discord.RichEmbed()
     .setDescription("Help Commands")
@@ -43,9 +36,11 @@ bot.on("message", async message => {
     .addField("Moderation","/kick [user] [reason] - Kick a User.\n/ban [user] [reason] - Ban a User. (soon)\n/unban [user ID] - unban a User.\n/report [user] [reason] - report about User.\n/mute [user] [reason] - Mute a User.\n/unmute [@user] [reason] - unMute a User.\n/warn [@user] [reason] - Warn a User.\n/clear [amount of messages] - Clear the chat.\n/say [message] - say your message.\n/poll [question] - poll about Question")
     .addField("Music","/play - Play  Music.\n/search - Search a Music.\n/skip - Skip a Music.\n/stop - Stop a Music.\n/volume - Volume the Music.\n/np - Show what song right now playing.\n/queue - Show what song right now playing.\n/pause - Pause the Music.\n/resume - Resume the Music.")
     .addField("Utilites","/serverinfo - Server Informations.\n/botinfo - Bot Informations.\n/membercount - Member Count.\n/avatar [@user] - Avatar of the user.\n/ping - Ping Pong.\n/staff - show you the staff member of WatchDog Bot Support DIscord.\n/vote - vote for the bot link.\n/invite - invite the bot link.");
-    return message.author.send(botembed);
+    return msg.author.send(botembed);
     //return message.channel.send("Check your Direct Messages!");
-  };
-});
- 
+  }
+
+  if(cmd === `${prefix}help`){ 
+    msg.reply("Check your DM!");
+
 bot.login(process.env.BOT_TOKEN);
