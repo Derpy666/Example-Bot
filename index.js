8a2da55ea6130f19a3a902921c94add9f39ed024
Derpy666 <37512753+Derpy666@users.noreply.github.com>
2019-07-09 13:17:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -38,24 +38,16 @@ bot.on("message", async message => {
     .split(" ");
   const command = args.shift().toLowerCase();
 
+  if (cmd === "ping") {
+// Normal Message
+message.channel.send("Pong!")
+  }
+  if (cmd === "hey") {
+// Reply Message
+message.reply("Hello!")
+  }
   if (cmd === "help") {
-    let botembed = new Discord.RichEmbed()
-      .setDescription("Help Commands")
-      .setColor(`RANDOM`)
-      .setThumbnail(bot.user.displayAvatarURL)
-      .addField(
-        "Moderation",
-        "/kick [user] [reason] - Kick a User.\n/ban [user] [reason] - Ban a User. (soon)\n/unban [user ID] - unban a User.\n/report [user] [reason] - report about User.\n/mute [user] [reason] - Mute a User.\n/unmute [@user] [reason] - unMute a User.\n/warn [@user] [reason] - Warn a User.\n/clear [amount of messages] - Clear the chat.\n/say [message] - say your message.\n/poll [question] - poll about Question"
-      )
-      .addField(
-        "Music",
-        "/play - Play  Music.\n/search - Search a Music.\n/skip - Skip a Music.\n/stop - Stop a Music.\n/volume - Volume the Music.\n/np - Show what song right now playing.\n/queue - Show what song right now playing.\n/pause - Pause the Music.\n/resume - Resume the Music."
-      )
-      .addField(
-        "Utilites",
-        "/serverinfo - Server Informations.\n/botinfo - Bot Informations.\n/membercount - Member Count.\n/avatar [@user] - Avatar of the user.\n/ping - Ping Pong.\n/staff - show you the staff member of WatchDog Bot Support DIscord.\n/vote - vote for the bot link.\n/invite - invite the bot link."
-      );
-    msg.reply("Check your DM!");
-    return msg.author.send(botembed);
+// DM Message
+message.author.send("your help commands")
   }
 });
