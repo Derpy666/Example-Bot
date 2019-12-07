de9463c170f51fb3d1051004299bf3e899e1eaa3
Derpy666 <37512753+Derpy666@users.noreply.github.com>
2019-12-07 22:23:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,53 +1,59 @@
-const Discord = require("discord.js");
-const bot = new Discord.Client({
-  disableEveryone: true
+const { Client, Collection, RichEmbed } = require("discord.js");
+const { readdirSync } = require("fs");
+const config = require("./config");
+const client = new Client({
+  disableEveryone: false,
+  fetchAllMembers: true
 });
-bot.login(process.env.BOT_TOKEN);
 
-const config = require("./config.js");
+client.login(config.token);
+
 const { prefix, token } = config;
 
+// Activity Function
 function Activity() {
-  return bot.user.setActivity(`${prefix}help | Made by Derpy`, { type: "PLAYING" });
+  return client.user.setActivity(`${prefix}help | Made by Derpy`, { type: "PLAYING" });
 }
 
-bot.on("ready", async () => {
+// Updates the bot's status when he online
+client.on("ready", async () => {
   console.log(`Bot is Online!`);
   Activity();
 });
 
 // Updates the bot's status if he joins a server
-bot.on("guildCreate", guild => {
+client.on("guildCreate", guild => {
   Activity();
 });
 
 /// Updates the bot's status if he leaves a servers
-bot.on("guildDelete", guild => {
+client.on("guildDelete", guild => {
   Activity();
 });
 
-bot.on("message", async message => {
+client.on("message", async message => {
   if (message.author.bot) return;
   if (message.channel.type === "dm") return;
 
   let msg = message.content.toLowerCase() || message.content.toUpperCase();
   if (!msg.startsWith(prefix)) return;
-  let args = message.content
-    .slice(bot.prefix.length)
-    .trim()
-    .split(" ");
+  let args = message.content.slice(bot.prefix.length).trim().split(" ");
   const command = args.shift().toLowerCase();
 
-  if (cmd === "ping") {
-// Normal Message
-message.channel.send("Pong!")
+  if (command === "ping") {
+message.channel.send("Pong!"); // Normal Message
   }
-  if (cmd === "hey") {
-// Reply Message
-message.reply("Hello!")
+  if (command === "hey") {
+message.reply("Hello!"); // Reply Message
   }
-  if (cmd === "help") {
-// DM Message
-message.author.send("your help commands")
+  if (command === "help") {
+message.author.send("your help commands"); // DM Message
+  }
+  if (command === "embed") {
+let embed = new RichEmbed()
+.setTitle("Title")
+.setDescription("Description");
+
+message.author.send(embed); // Embed Message
   }
 });
